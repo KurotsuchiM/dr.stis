0b0a1df5f9ac9527b830cce0c1fb377893d8c4a7
Fazziza <fachrunisaazziza22@gmail.com>
2018-12-13 06:34:52 +0700

    commit apa

---
+++ app/src/main/java/stis/kelompok4/drstis/Formulir.java
@@ -0,0 +1,36 @@
+package stis.kelompok4.drstis;
+
+public class Formulir extends AppCompatActivity {
+
+	EditText reservasi_nama, reservasi_tanggal, reservasi_jam, keluhan;
+	Button submit;
+	RequestQueue requestQueue;
+	String insertUrl = "http://192.168.100.6/res/insert_form.php";
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.formulir);
+		//formulir layout itu buat sementara jadi nanti diganti yang bener
+
+		reservasi_nama = (EditText) findViewById(R.id.editText);
+		reservasi_tanggal = (EditText) findViewById(R.id.editText2);
+		reservasi_jam = (EditText) findViewById(R.id.editText3);
+		submit = (Button) findViewById(R.id.submit);
+
+		requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+	insert.setOnClickListener(new View.OnClickListener() {
+		public void onClick(View view) {
+			StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>(){
+				@Override
+				public void onResponse(String response) {
+					System.out.println(response.toString());
+				}
+			}, new Response)
+		}
+	})
+
+	}
+}	

cd42c8be05a8f88cd5bedee7a0a30a8a86c68ca3
alfiankhusnul <alfiankhusnul@gmail.com>
2018-12-12 21:42:54 +0700

    add formulir UI

---
+++ app/src/main/java/stis/kelompok4/drstis/FormulirActivity.java
@@ -0,0 +1,15 @@
+package stis.kelompok4.drstis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormulirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulir);
+    }
+
+
+}
--- app/src/main/java/stis/kelompok4/drstis/LoginActivity.java
+++ app/src/main/java/stis/kelompok4/drstis/LoginActivity.java
@@ -2,12 +2,21 @@ package stis.kelompok4.drstis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private EditText usernameInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        usernameInput = (EditText) findViewById(R.id.username_Input);
+
     }
 }

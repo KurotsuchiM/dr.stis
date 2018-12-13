da7d5a70648ba13e7c642ed9f126186bf7bc3d1c
Fazziza <fachrunisaazziza22@gmail.com>
2018-12-13 09:09:05 +0700

    folder ci

---
+++ ci-restserver-master/application/models/Reservasi.php
@@ -0,0 +1,74 @@
+<?php 
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 
+ */
+class reservasi extends CI_Model
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->tabel='reservasi';
+	}
+
+	public function getList($reservasi_nim) {
+		if(!$reservasi_nim) return;
+		$this->db->where('reservasi_nim',$reservasi_nim);
+		$query = $this->db->get('reservasi');
+		return $query->result_array();
+	}
+
+	public function getReservasi($reservasi_nim) {
+		if(!$reservasi_nim) {
+			return;
+		}else{
+			$query =$this->db->select('SELECT p.pengunjung_nama, r.reservasi_nim,r.reservasi_dokter_nip, d.dokter_nama, reservasi_tanggal, reservasi_jam, keluhan FROM reservasi r INNER JOIN pengunjung p ON r.reservasi_nim=p.pengunjung_nim INNER JOIN dokter d ON d.dokter_nip=r.reservasi_dokter_nip WHERE r.reservasi_nim=$reservasi_nim GROUP BY p.pengunjung_nim');
+			return $query->result_array();
+		}
+	}
+
+	public function delete($reservasi_nim) {
+		$query = $this->db->query("DELETE FROM reservasi WHERE reservasi_nim=$reservasi_nim AND reservasi_tanggal=$reservasi_tanggal");
+		return TRUE;
+	}
+
+	public function get_user_by_id($pengunjung_nim){
+		$this->db->where('pengunjung_nim',$pengunjung_nim);
+		$query = $this->db->get('pengunjung');
+		return $query->result_array();
+	}
+
+	public function get_dokter_by_id($reservasi_dokter_nip,$reservasi_tanggal) {
+
+		$this->db->where('dokter_nip',$reservasi_dokter_nip);
+		$query = $this->db->get('dokter');
+		return $query->result_array();
+	}
+
+	public function insertReservasi($content){
+		$tanggal = date('d', $content['reservasi_tanggal']);
+		if($tanggal < '15') {
+			$reservasi_dokter_nip = '199705042021011935';
+		}else{
+			$reservasi_dokter_nip = '19980406202102356';
+		}
+		$content['reservasi_dokter_nip']=$reservasi_dokter_nip;
+		
+		if(!$content) return;
+		$data = array(
+			'pengunjung_nim' => $content['pengunjung_nim'],
+			'reservasi_dokter_nip' => $content['reservasi_dokter_nip'],
+			'reservasi_tanggal' => $content['reservasi_tanggal'],
+			'reservasi_jam' =>$content['reservasi_jam'],
+			'keluhan' => $content['keluhan']
+		);
+
+		$this->db->insert('reservasi', $data);
+
+		return $content;
+	}
+}
+ ?>
--- res/dr_stis_tes.sql
+++ res/dr_stis_tes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 12, 2018 at 01:41 AM
+-- Generation Time: Dec 13, 2018 at 12:52 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dr.stis`
+-- Database: `dr.stis_tes`
 --
 
 -- --------------------------------------------------------
@@ -43,8 +43,32 @@ CREATE TABLE `dokter` (
 --
 
 INSERT INTO `dokter` (`dokter_nip`, `dokter_nama`, `spesifikasi`, `dokter_email`, `dokter_alamat`, `dokter_notelp`, `password`) VALUES
-('199705042021011935', 'Kurotsuchi', 'Umum', 'mayuri@gmail.com', 'kramat jati', '082280627393', '123456'),
-('19980406202102356', 'Soi Fon', 'Jantung', 'fon@gmail.com', 'distrik 13', '086549643546', '123456');
+('199705042021011935', 'Kurotsuchi ', 'Umum', 'mayuri@gmail.com', 'kramat jati', '082280627393', '123456'),
+('19980406202102356', 'Abu', 'Jantung', 'fon@gmail.com', 'distrik 13', '086549643546', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kunci`
+--
+
+CREATE TABLE `kunci` (
+  `id` int(11) NOT NULL,
+  `pengunjung_id` varchar(50) NOT NULL,
+  `key` varchar(40) NOT NULL,
+  `level` int(2) NOT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT '0',
+  `is_private_key` tinyint(1) NOT NULL DEFAULT '0',
+  `alamat_ip` text,
+  `tanggal_buat` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `kunci`
+--
+
+INSERT INTO `kunci` (`id`, `pengunjung_id`, `key`, `level`, `ignore_limits`, `is_private_key`, `alamat_ip`, `tanggal_buat`) VALUES
+(1, '16.9395', 'CODEX@123', 0, 0, 0, NULL, '2018-10-11 13:34:33');
 
 -- --------------------------------------------------------
 
@@ -54,21 +78,26 @@ INSERT INTO `dokter` (`dokter_nip`, `dokter_nama`, `spesifikasi`, `dokter_email`
 
 CREATE TABLE `pengunjung` (
   `pengunjung_nama` varchar(50) NOT NULL,
-  `pengujung_nim` varchar(20) NOT NULL,
+  `pengunjung_nim` varchar(20) NOT NULL,
   `status` varchar(15) NOT NULL,
   `password` varchar(256) NOT NULL,
   `email` varchar(50) NOT NULL,
   `nomor_telpon` varchar(14) NOT NULL,
-  `jenis_kelamin` varchar(10) NOT NULL
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `tanggal_buat` datetime NOT NULL,
+  `modifikasi` datetime NOT NULL,
+  `status_akun` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Aktif | 0 = Tidak Aktif'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pengunjung`
 --
 
-INSERT INTO `pengunjung` (`pengunjung_nama`, `pengujung_nim`, `status`, `password`, `email`, `nomor_telpon`, `jenis_kelamin`) VALUES
-('Alfian', '16.9000', 'Mahasiswa', '1213456', 'alfi@gmail.com', '081634982786', 'Laki-laki'),
-('Mr. X', '19722200012414978', 'Dosen', '987654', 'suef@gmail.com', '08125487645', 'Laki-laki');
+INSERT INTO `pengunjung` (`pengunjung_nama`, `pengunjung_nim`, `status`, `password`, `email`, `nomor_telpon`, `jenis_kelamin`, `tanggal_buat`, `modifikasi`, `status_akun`) VALUES
+('Alfian', '16.9000', 'Mahasiswa', '1213456', 'alfi@gmail.com', '081634982786', 'Laki-laki', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1),
+('Anonim2', '16.9308', 'mahasiswa', 'opf324', 'min@stis.ac.id', '089569550897', 'p', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1),
+('Anonim1', '16.9395', 'mahasiswa', '12f324', 'anom@stis.ac.id', '0895695505968', 'l', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1),
+('Mr. X', '19722200012414978', 'Dosen', '987654', 'suef@gmail.com', '08125487645', 'Laki-laki', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -91,7 +120,11 @@ CREATE TABLE `reservasi` (
 
 INSERT INTO `reservasi` (`reservasi_no`, `reservasi_nim`, `reservasi_dokter_nip`, `reservasi_tanggal`, `reservasi_jam`, `keluhan`) VALUES
 (1, '16.9395', '199705042021011935', '2018-12-09', '23:00:00', 'sakit perut'),
-(2, '16.9308', '199705042021011935', '2018-12-11', '06:00:00', 'maag');
+(2, '16.9308', '199705042021011935', '2018-12-11', '06:00:00', 'maag'),
+(3, '', '19980406202102356', '0000-00-00', '00:00:00', ''),
+(4, '', '19980406202102356', '0000-00-00', '00:00:00', ''),
+(5, '', '19980406202102356', '0000-00-00', '00:00:00', ''),
+(6, '', '19980406202102356', '0000-00-00', '00:00:00', '');
 
 --
 -- Indexes for dumped tables
@@ -104,39 +137,38 @@ ALTER TABLE `dokter`
   ADD PRIMARY KEY (`dokter_nip`);
 
 --
+-- Indexes for table `kunci`
+--
+ALTER TABLE `kunci`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `pengunjung`
 --
 ALTER TABLE `pengunjung`
-  ADD PRIMARY KEY (`pengujung_nim`);
+  ADD PRIMARY KEY (`pengunjung_nim`);
 
 --
 -- Indexes for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  ADD PRIMARY KEY (`reservasi_no`),
-  ADD KEY `reservasi_dokter_nip` (`reservasi_dokter_nip`),
-  ADD KEY `reservasi_nim` (`reservasi_nim`) USING BTREE;
+  ADD PRIMARY KEY (`reservasi_no`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `kunci`
+--
+ALTER TABLE `kunci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `reservasi_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `reservasi`
---
-ALTER TABLE `reservasi`
-  ADD CONSTRAINT `fk_reservasi_dokter_nip` FOREIGN KEY (`reservasi_dokter_nip`) REFERENCES `dokter` (`dokter_nip`),
-  ADD CONSTRAINT `reservasi_ibfk_1` FOREIGN KEY (`reservasi_dokter_nip`) REFERENCES `dokter` (`dokter_nip`);
+  MODIFY `reservasi_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

0f88bc3548e960a5303a750fffb58f6a06b462ec
Krishna Aji S <stereoxas@gmail.com>
2018-12-15 01:44:30 +0700

    Calendar update
    1. Added partial Pesan Jadwal support

--- app/src/main/java/stis/kelompok4/drstis/activity_jadwal.java
+++ app/src/main/java/stis/kelompok4/drstis/activity_jadwal.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,18 +21,22 @@ import static java.lang.Boolean.FALSE;
 public class activity_jadwal extends AppCompatActivity implements ActivityJadwalAdapter.ItemClickListener
 {
 
+    private boolean selectedTimeStatusAvailable = false;
     private String selectedDate;
     private TextView selectedDateText;
     private ActivityJadwalAdapter adapter;
+    private String selectedTime;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwal);
 
         selectedDateText = (TextView) findViewById(R.id.selectedDate) ;
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
+        Button pesanJadwal = findViewById(R.id.jadwal_button);
 
         //ArrayList<String> listWaktu = new ArrayList<>();
         String listWaktu[] = new String[15];
@@ -51,23 +56,6 @@ public class activity_jadwal extends AppCompatActivity implements ActivityJadwal
         listWaktu[13] = "15:30";
         listWaktu[14] = "16:00";
 
-        HashMap<String, String> listAvailable = new HashMap<>();
-        listAvailable.put("jam0900", "09:00");
-        listAvailable.put("jam0930", "09:30");
-        listAvailable.put("jam1000", "10:00");
-        listAvailable.put("jam1030", "10:30");
-        listAvailable.put("jam1100", "11:00");
-        listAvailable.put("jam1130", "11:30");
-        listAvailable.put("jam1200", "12:00");
-        listAvailable.put("jam1230", "12:30");
-        listAvailable.put("jam1300", "13:00");
-        listAvailable.put("jam1330", "13:30");
-        listAvailable.put("jam1400", "14:00");
-        listAvailable.put("jam1430", "14:30");
-        listAvailable.put("jam1500", "15:00");
-        listAvailable.put("jam1530", "15:30");
-        listAvailable.put("jam1600", "16:00");
-
 
         RecyclerView listView = (RecyclerView) findViewById(R.id.jadwal_list_itself);
         listView.setLayoutManager(new GridLayoutManager(this, 3));
@@ -84,12 +72,36 @@ public class activity_jadwal extends AppCompatActivity implements ActivityJadwal
             selectedDateText.setText(getSelectedDate());
         }
 
+        pesanJadwal.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                setSelectedTime("");
+
+                Intent intent1 = new Intent(getApplicationContext(), FormulirActivity.class);
+                intent1.putExtra("Jam", getSelectedTime());
+                intent1.putExtra("Tanggal", getSelectedDate());
+
+                startActivity(intent1);
+
+            }
+        });
+
     }
 
     @Override
     public void onItemClick(View view, int position)
     {
-        Toast.makeText(this, "Klik " + adapter.getItem(position) + " pada " + position, Toast.LENGTH_SHORT).show();
+        if(isSelectedTimeStatusAvailable())
+        {
+            Toast.makeText(this, "Jam " + adapter.getItem(position) + " tersedia untuk dipesan.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Jam " + adapter.getItem(position) + " tidak tersedia, mohon pilih jam yang lain.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public String getSelectedDate() {
@@ -98,5 +110,21 @@ public class activity_jadwal extends AppCompatActivity implements ActivityJadwal
 
     public void setSelectedDate(String selectedDate) {
         this.selectedDate = selectedDate;
+    }
+
+    public boolean isSelectedTimeStatusAvailable() {
+        return selectedTimeStatusAvailable;
+    }
+
+    public void setSelectedTimeStatusAvailable(boolean selectedTimeStatus) {
+        this.selectedTimeStatusAvailable = selectedTimeStatus;
+    }
+
+    public String getSelectedTime() {
+        return selectedTime;
+    }
+
+    public void setSelectedTime(String selectedTime) {
+        this.selectedTime = selectedTime;
     }
 }
